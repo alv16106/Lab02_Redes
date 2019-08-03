20a5ff6a2d9b7eefe65eb0fd6f80c60e1f94f753
Javos <ram16230@uvg.edu.gt>
2019-08-02 20:10:41 -0600

    push

--- sender.py
+++ sender.py
@@ -1,17 +1,19 @@
-import socket, pickle
+import socket
 import bitarray
+import pickle
+
 
 def main():
-  info = bitarray.bitarray('1001101')
-  pickeld = pickle.dumps(info)
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.connect(('localhost', 8080))
-  sock.send(pickeld)
-  ## Receive up to 4096 bytes from a peer
-  sock.recv(1024)
-  ## Close the socket connection, no more data transmission
-  sock.close()
+    info = bitarray.bitarray('1001101')
+    pickeld = pickle.dumps(info)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', 8080))
+    sock.send(pickeld)
+    # Receive up to 4096 bytes from a peer
+    sock.recv(1024)
+    # Close the socket connection, no more data transmission
+    sock.close()
 
 
 if __name__ == "__main__":
-  main()
+    main()
